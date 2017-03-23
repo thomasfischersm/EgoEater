1a66cebb8ac8598ab9b8fede38afeff88485914c
thomasfischersm <thomas@playposse.com>
2017-03-22 19:15:26 -0700

    Enabled highest photo quality in Glide.

---
+++ app/src/main/java/com/playposse/egoeater/glide/EgoEaterGlideModule.java
@@ -0,0 +1,24 @@
+package com.playposse.egoeater.glide;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.module.GlideModule;
+
+/**
+ * A configuration class for the Glide library.
+ */
+public class EgoEaterGlideModule implements GlideModule {
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        // Enable the highest photo quality.
+        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide) {
+        // Nothing to do.
+    }
+}
