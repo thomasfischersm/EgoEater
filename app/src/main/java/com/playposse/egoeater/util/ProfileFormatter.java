c1220f0b80ad9f1d1c20e85a49646925d11b9d14
thomasfischersm <thomas@playposse.com>
2017-12-16 13:36:39 -0800

    Switch profile builder segment separator from / to |.

--- app/src/main/java/com/playposse/egoeater/util/ProfileFormatter.java
+++ app/src/main/java/com/playposse/egoeater/util/ProfileFormatter.java
@@ -28,13 +28,6 @@ public final class ProfileFormatter {
     }
 
     public static String formatCityStateAndDistance(Context context, ProfileParcelable profile) {
-        if (StringUtil.isEmpty(profile.getCity())
-                || StringUtil.isEmpty(profile.getState())
-                || StringUtil.isEmpty(profile.getCountry())) {
-            // Skip. The data is incomplete.
-            return "";
-        }
-
         int distance = (int) profile.getDistance();
         final String distanceStr;
         if (distance > 0) {
@@ -43,10 +36,25 @@ public final class ProfileFormatter {
             distanceStr = "";
         }
 
-        if (profile.getCountry().equals(USA_COUNTRY)) {
+        boolean hasCity = !StringUtil.isEmpty(profile.getCity());
+        boolean hasState = !StringUtil.isEmpty(profile.getState());
+        boolean hasCountry = !StringUtil.isEmpty(profile.getCountry());
+        boolean isUsa = USA_COUNTRY.equals(profile.getCountry());
+
+        if (hasCity && hasState && isUsa) {
             return profile.getCity() + LOCATION_SEPARATOR + profile.getState() + distanceStr;
-        } else {
+        } else if (hasCity && hasCountry){
             return profile.getCity() + LOCATION_SEPARATOR + profile.getCountry() + distanceStr;
+        } else if (hasState && hasCountry) {
+            return profile.getState() + LOCATION_SEPARATOR + profile.getCountry() + distanceStr;
+        } else if (hasCountry) {
+            return profile.getCountry() + distanceStr;
+        } else if (hasCity) {
+            return profile.getCity() + distanceStr;
+        } else if (hasState) {
+            return profile.getState() + distanceStr;
+        } else {
+            return distanceStr.trim();
         }
     }
 
